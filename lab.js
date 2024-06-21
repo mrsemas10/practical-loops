f28980098ec8d892188900211298b34286da9bc0
mrsemas10 <mrsemas10@gmail.com>
2024-06-21 17:11:11 -0400

    part 1 and 2 complete

---
+++ lab.js
@@ -0,0 +1,29 @@
+// Part 1: Fizz Buzz
+
+for (let i=1; i<=100; i++) {
+    if (i%3 === 0 && i%5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (i%5 === 0) {
+        console.log("Buzz");
+    }
+    else if (i%3 === 0) {
+        console.log("Fizz");
+    }
+    else {
+        console.log(i);
+    }
+} 
+
+// Part 2: Prime Time
+
+
+for (let n=1; n<=30; n++) {
+     // prints prime for prime numbers
+     if (n === 5 || n === 7 || n === 11 || n === 13 || n === 17 || n === 19 || n === 23 || n === 29) {
+        console.log( n + ' is prime');
+     }
+    }
+
+// Part 3: Feeling Loopy I dont really understand what I am supposed to do here
+
